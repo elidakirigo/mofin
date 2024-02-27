c0e564efb740f9e4e5e1eda168d70bc1f891ce83
elida <elidakirigo@gmail.com>
2024-02-27 13:00:00 +0300

    core:adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
+	return (
 		<div className='App'>
 			<h1 className='text-3xl font-bold underline '>Hello world!</h1>
 		</div>
-  )
+	)
 }
 
-export default App;
+export default App
